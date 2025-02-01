d4e8a8396c42f0e37ed717ebe88575b80f860462
Kukatla Teja <tejaspix1@gmail.com>
2025-02-01 22:36:50 +0530

    changed apiPath

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL="http://localhost:4000"
+// export const API_URL="http://localhost:4000"
 
-//export const API_URL="https://backend-multivendor-3zna.onrender.com"
+export const API_URL="https://backend-multivendor-3zna.onrender.com"
